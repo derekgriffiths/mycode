26ba8107df8a467c9d8db09638d339f91461fc68
derekgriffiths <derekgriffiths@alta3.com>
2020-05-04 14:58:07 -0400

    updated my stuff up to lab 8

---
+++ lab_input/challenge_lab01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+name = input("What is your name? ")
+
+day = input("What day is today? ")
+
+print("Hello, " + name + "! Happy " + day + "!")
+
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# creates a list
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+# displays the first item
+print("The first item in the list (IP): " + my_list[0] )
+
+# displays the second item
+print("The first item in the list (IP): " + str(my_list[1]) )
+
+# displays the third item
+print("The first item in the list (IP): " + my_list[2] )
+
+# new list that I'm creating
+new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# printing info relating to my "new list"
+print("When I " + new_list[5] + " into IP addresses " + new_list[3] + " or " + new_list[4] + " I am unable to ping ports " + str(new_list[0]) + "," + str(new_list[1]) + " or " + str(new_list[2]) + ".")
+
+
