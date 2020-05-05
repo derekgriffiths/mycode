2bea0671202495fa2c532018603f88aaf0e11b11
derekgriffiths <derekgriffiths@alta3.com>
2020-05-04 22:13:14 -0400

    End of day updates on training Day 1

---
+++ lab_input/challenge_lab02.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+col_list = ["blue", "Columbus"]
+
+col_list.append(1492)
+
+user_input = input("What is your name?")
+
+print("In " + str(col_list[2]) + ", " + col_list[1] + " sailed the ocean " + col_list[0] + ". " + user_input + " fell off the boat.")
+
+
+
+
+
+
