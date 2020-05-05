0e0c8371ab1770298df7f2972f2ca94d9672814a
derekgriffiths <derekgriffiths@alta3.com>
2020-05-05 14:01:57 -0400

    Update to if elif task 2

--- iftest2/ipask2.py
+++ iftest2/ipask2.py
@@ -6,7 +6,7 @@ ipchk = input("Apply an IP address: ") # this line now prompts the user for inpu
 
 if ipchk == "192.168.70.1":
    print("Looks like the IP address of the Gateway was set: " + ipchk + " This is not recommended.")
-elif ipchk != "192.168.70.1": # if any data is provided other than the 192 IP address, this will test true
+elif ipchk: # if any data is provided other than the 192 IP address, this will test true
    print("Looks like the IP address was set: " + ipchk) # indented under if
 else: # if data is NOT provided
    print("You did not provide input.") # indented under else
