bbbb376d7cad6776091299d8faf04615bdbe38fb
derekgriffiths <derekgriffiths@alta3.com>
2020-05-05 13:22:35 -0400

    testing if statement

---
+++ iftest/condition02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Asks for user input to define hostname
+hostname = input("What value should we set for hostname?")
+
+# If the user input matches the hostname, the text below will be output
+if hostname == "MTG":
+    print("The hostname was found to be mtg")
+
---
+++ iftest/condition03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Asking user for input to define the hostname
+hostname = input("What value should we set for hostname?")
+
+## Notice how the next line has changed
+## here we use the str.lower() method to return a lowercase string
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+    print("Hostname matches expected config") # Added a second line to output if the user enters the matches text
+
+# Output at the end of the script every time
+print("Exiting the script")
