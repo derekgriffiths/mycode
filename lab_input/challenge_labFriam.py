1bb3088e3296ebb52550e43f1a66fc21123c778c
derekgriffiths <derekgriffiths@alta3.com>
2020-05-08 12:54:44 -0400

    Snapshot near end of day Friday

---
+++ lab_input/challenge_labFriam.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+astro= {"number": 3, "message": "success", "people": [{"craft": "ISS", "name": "Chris Cassidy"}, {"craft": "ISS", "name": "Anatoly Ivanishin"}, {"craft": "ISS", "name": "Ivan Vagner"}]}
+
+print("People in space: " + str(astro["number"]))
+
+# for anaut in astro:
+#    print(anaut["message"])
+# see the screen snapshots I took which show solutions
+
+
+
+
